38164e611a007d97cbc239f5fc28a78d4402f685
Tran <tranle97@gmail.com>
2021-09-16 14:42:21 -0500

    Create light and dark toggle mode

--- index.js
+++ index.js
@@ -3,15 +3,30 @@ const apps = ['app', 'blog', 'shop', 'note', 'game', 'grave'];
 let appName = document.getElementById('app-name');
 appName.innerText = apps[0];
 
+const toggleDark = () => {
+  document.body.style.color = '#ffedd9';
+  document.body.style.backgroundColor = '#000000';
+  appName.style.color = '#ffc88c';
+};
+
+const toggleLight = ()  => {
+  document.body.style.color = '#000000';
+  document.body.style.backgroundColor = '#FFFFFF';
+  appName.style.color = '#8e8cff';
+};
+
 let rotateRight = () => {
   apps.push(apps.shift());
   appName.innerText = apps[0];
+  apps[0] === 'grave' ? toggleDark() : toggleLight();
   return apps;
 };
 
 let rotateLeft = () => {
   apps.unshift(apps.pop());
   appName.innerText = apps[0];
+  console.log(apps[0]);
+  apps[0] === 'grave' ? toggleDark() : toggleLight();
   return apps;
 };
 
